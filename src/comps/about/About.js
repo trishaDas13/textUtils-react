4abc6f5f54bde7c9569e5dc2801d8d9fba422c99
trishaDas13 <trisha.das1308@gmail.com>
2024-01-15 23:23:55 +0530

    contact page done

--- src/comps/about/About.js
+++ src/comps/about/About.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './about.css';
+import  Data  from './Data';
 
 function About() {
   return (
-    <div>About</div>
+    <section className="about">
+      {
+        Data.map((item, idx) => {
+          return (
+            <div className="sector" key={idx}>
+              <div className="heading">
+                <h3>{item.heading}</h3>
+                <button>+</button>
+              </div>
+              <p>{item.content}</p>
+            </div>
+          );
+        })
+        
+      }
+    </section>
   )
 }
 
-export default About
+export default About;
--- src/comps/contact/Contact.js
+++ src/comps/contact/Contact.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './contact.css'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <section className="contact">
+      <div className="container">
+        <h1>Contact Me</h1>
+        <p>Created by : Trisha Das</p>
+        <p>Email Address : trisha.das1308@gmail.com</p>
+        <p>Linked in link : <a href="https://www.linkedin.com/in/trisha-das1308/" target='_blank'> https://www.linkedin.com/in/trisha-das1308/ </a></p>
+      </div>
+    </section>
   )
 }
 
-export default Contact
+export default Contact;
